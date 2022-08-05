9fea98280d6b8ec758002391b92e9823df9128e5
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-08-04 18:20:48 -0800

    add plain code for species number targets and "top cpue" table

--- R/make_tabs.R
+++ R/make_tabs.R
@@ -5,16 +5,65 @@
 # Table 1 is the target sample sizes for different species categories
 # Table 2 is the number of stations allocated, attempted, and successfully completed.
 # Table 3 onwards are specific to each species.
-targetn <- read.csv("data/TargetN.csv") # for now, it's a csv
-sampled_stations <- data.frame(INPFC_area = c("Shumagin","Chirikof"), 
-                               Stations_allocated = c(74, 32)) # there is sql code for this in sql/
-top_CPUE <- make_top_cpue()
+# targetn <- read.csv("data/TargetN.csv",header = TRUE) # csv was corrupted
+targetn <- data.frame(
+          stringsAsFactors = FALSE,
+             Species.or.species.group = c("Walleye pollock",
+                                          "Pacific cod","Arrowtooth flounder (ATF)",
+                                          "All rockfish species","Sablefish",
+                                          "Atka mackerel",
+                                          "All species of flatfish (except ATF)",
+                                          "Skates and Sharks (total length)",
+                                          "Grenadiers (tip of snout to insertion of first anal ray)",
+                                          "Prowfish","Lingcod","Salmon",
+                                          "Yellow Irish lord (Hemilepidotus jordani)",
+                                          "Bigmouth sculpin (Hemilepidotus bolini)",
+                                          "Great sculpin (Myoxocephalus polyacnthocephalus)",
+                                          "Plain sculpin (Myoxocephalus jaok)",
+                                          "Warty sculpin (Myoxocephalus verrucosus)",
+                                          "Forage fish (herring, eulachon, capelin, sand lance)",
+                                          "Commander squid (Berryteuthis magister)"),
+                   Target.sample.size = c("100","100","150","100",
+                                          "100","100","100","50","50","All",
+                                          "All","All","All","All","All",
+                                          "All","All","All","All")
+           )
+sampled_stations <- data.frame(INPFC_area = c("Shumagin","Chirikof", "Kodiak","Yakutat","Southeastern", "All areas"), 
+                               Stations_allocated = NA) # there is sql code for this in sql/
+
+if(SRVY=="GOA"){
+  dat <- read.csv("data/goa_strata.csv",header= TRUE)
+  region_lu <- dat %>% 
+    filter(SURVEY=="GOA") %>%
+    dplyr::select(SURVEY, STRATUM, INPFC_AREA) 
+}
+
+# most_abundant <- cpue_raw %>% 
+#   dplyr::filter(srvy=="GOA" & year == YEAR) %>%
+#   dplyr::group_by(common_name) %>%
+#   dplyr::summarize(totaln = sum(count, na.rm = TRUE)) %>%
+#   top_n(n = 11)
+  
+make_top_cpue <- function(YEAR, SRVY){
+  cpue_raw %>% 
+    filter(year==YEAR & srvy==SRVY) %>%
+    dplyr::mutate(taxon = dplyr::case_when(
+      species_code <= 31550 ~ "fish", 
+      species_code >= 40001 ~ "invert")) %>%
+    dplyr::filter(taxon == "fish") %>%
+    left_join(region_lu, by=c('stratum'='STRATUM')) %>%
+    dplyr::group_by(INPFC_AREA, common_name) %>%
+    dplyr::summarize(mean_cpue = mean(cpue_kgkm2)) %>%
+    top_n(n = 20) %>%
+    dplyr::ungroup()
+}
+
+top_CPUE <- make_top_cpue(YEAR = YEAR, SRVY = SRVY)
 
 list_tables <- list()
 list_tables[[1]] <- targetn
 list_tables[[2]] <- sampled_stations
-list_tables[[3]] <- 
-
+list_tables[[3]] <- top_CPUE
 
 save(list_tables,
      file = "figtab/report_tables.rdata"
