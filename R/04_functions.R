ce82769eb43fbccacb624272f276f134ad523ec4
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-09-01 12:33:59 -0700

    add palette lengthening fn

--- R/04_functions.R
+++ R/04_functions.R
@@ -2924,3 +2924,18 @@ make_png <- function(fig_list_element,
   fig_list_element
   dev.off()
 }
+
+#' Lengthen a color palette
+#'
+#' @param x a vector of 1:n, where n is the number of unique colors you want
+#' @param shortpal a short color palette, e.g., one from RColorBrewer()
+#'
+#' @return a vector of the same length as x, with n unique colors
+#' @export
+#'
+#' @examples
+lengthen_pal <- function(x = 1:10, shortpal) {
+  ncolours <- length(unique(x))
+  newpal <- colorRampPalette(shortpal)(ncolours)
+  return(newpal)
+}
