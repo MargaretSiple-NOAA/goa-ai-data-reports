acaf70a0ec0d2c5133ad41f1f21c25d4f3d6469f
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-10-06 09:19:20 -0700

    Clean up data download file and add messages (table downloads can take a while)

--- R/05_download_data_from_oracle.R
+++ R/05_download_data_from_oracle.R
@@ -7,31 +7,20 @@ if (!file.exists("data/local_nodc")) dir.create("data/local_nodc", recursive = T
 if (!file.exists("data/local_ai")) dir.create("data/local_ai", recursive = TRUE)
 if (!file.exists("data/local_goa")) dir.create("data/local_goa", recursive = TRUE)
 
-# Load packages -----------------------------------------------------------
-PKG <- c("RODBC", "getPass")
-for (p in PKG) {
-  if (!require(p, character.only = TRUE)) {
-    install.packages(p)
-    require(p, character.only = TRUE)
-  }
-}
 
-# Secret database login info:
+# Setup channel to connect to Oracle --------------------------------------
+
 source("R/setup_channel.R")
 
-# The setup_channel.R script sets up a channel using your Oracle username and pw:
-# channel <- odbcConnect(dsn = "AFSC",
-#                        uid = "", # change
-#                        pwd = "", #change
-#                        believeNRows = FALSE)
-#
-# odbcGetInfo(channel)
+# The setup_channel.R script sets up a channel using your Oracle username and pw. 
 
 ################## DOWNLOAD TABLES##########################################
 # RACEBASE ----------------------------------------------------------------
 
 a <- RODBC::sqlQuery(channel, "SELECT * FROM RACEBASE.CATCH")
 write.csv(x = a, "./data/local_racebase/catch.csv", row.names = FALSE)
+
+print("Finished downloading CATCH.")
 
 a <- RODBC::sqlQuery(channel, "SELECT * FROM RACEBASE.HAUL")
 a <- RODBC::sqlQuery(
@@ -46,8 +35,12 @@ a <- RODBC::sqlQuery(
 )
 write.csv(x = a, "./data/local_racebase/haul.csv", row.names = FALSE)
 
+print("Finished downloading HAUL.")
+
 a <- RODBC::sqlQuery(channel, "SELECT * FROM RACEBASE.LENGTH")
 write.csv(x = a, "./data/local_racebase/length.csv", row.names = FALSE)
+
+print("Finished downloading LENGTH.")
 
 a <- RODBC::sqlQuery(channel, "SELECT * FROM RACEBASE.SPECIMEN")
 write.csv(x = a, "./data/local_racebase/specimen.csv", row.names = FALSE)
@@ -67,6 +60,7 @@ write.csv(x = a, "./data/local_racebase/cruise.csv", row.names = FALSE)
 a <- RODBC::sqlQuery(channel, "SELECT * FROM RACEBASE.SPECIES_CLASSIFICATION")
 write.csv(x = a, "./data/local_racebase/species_classification.csv", row.names = FALSE)
 
+print("Finished downloading SPECIMEN, STRATUM, SPECIES, CRUISE and SPECIES_CLASSIFICATION.")
 
 # RACE_DATA ---------------------------------------------------------------
 
@@ -88,6 +82,7 @@ write.csv(x = a, "./data/local_race_data/species_taxonomic.csv", row.names = FAL
 a <- RODBC::sqlQuery(channel, "SELECT * FROM RACE_DATA.V_CRUISES")
 write.csv(x = a, "./data/local_race_data/cruises.csv", row.names = FALSE)
 
+print("Finished downloading RACE_DATA tables. We may not need all of these forever.")
 
 
 # ADFG --------------------------------------------------------------------
@@ -98,6 +93,7 @@ write.csv(x = a, "./data/length_ADFG.csv", row.names = FALSE)
 a <- RODBC::sqlQuery(channel, "SELECT * FROM RACEBASE.SPECIMEN_ADFG")
 write.csv(x = a, "./data/specimen_ADFG.csv", row.names = FALSE)
 
+print("Finished downloading ADF&G tables.")
 
 
 # GOA ---------------------------------------------------------------------
@@ -110,6 +106,9 @@ write.csv(x = a, "./data/local_goa/goa_biomass_stratum.csv", row.names = FALSE)
 
 a <- RODBC::sqlQuery(channel, "SELECT * FROM GOA.STATION_ALLOCATION")
 write.csv(x = a, "./data/local_goa/goa_station_allocation.csv", row.names = FALSE)
+
+print("Finished downloading GOA schema tables.")
+
 
 # AI ----------------------------------------------------------------------
 a <- RODBC::sqlQuery(channel, "SELECT * FROM AI.BIOMASS_TOTAL")
@@ -124,6 +123,12 @@ write.csv(x = a, "./data/local_ai/ai_station_allocation.csv", row.names = FALSE)
 a <- RODBC::sqlQuery(channel, "SELECT * FROM AI.CPUE")
 write.csv(x = a, "./data/local_ai/cpue.csv", row.names = FALSE)
 
+print("Finished downloading AI schema tables.")
+
+
 # NODC (Food Habits) ------------------------------------------------------
-a <- RODBC::sqlQuery(channel, "SELECT * FROM FOODLAB.NODC")
-write.csv(x = a, "./data/local_nodc/nodc.csv", row.names = FALSE)
+# a <- RODBC::sqlQuery(channel, "SELECT * FROM FOODLAB.NODC")
+# write.csv(x = a, "./data/local_nodc/nodc.csv", row.names = FALSE)
+# 
+
+print("Finished downloading local tables! Yay.")
