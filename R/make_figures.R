38e715e19ca369757bb493e1a6c9f04331a10b25
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-10-07 09:58:28 -0700

    move figures from presentation file to make_figures

--- R/make_figures.R
+++ R/make_figures.R
@@ -2,13 +2,347 @@
 
 # Figures -----------------------------------------------------------------
 # Fig 1: district map
-# Fig 2. CPUE map 1
-# Fig 3. size comp plot 1
+# Fig 2. CPUE map 1 "cpue_bubbles"
+# Fig 3. size comp plot 1 "joy_division_length"
 # Fig 4. CPUE map 2
 # Fig 5. size comp plot 2
 
-# Static figure: INPFC areas ----------------------------------------------
-# 
+make_biomass_timeseries <- TRUE
+# 2. Catch composition plot
+make_catch_comp <- TRUE
+# 3. CPUE bubble maps
+make_cpue_bubbles <- TRUE
+# 5. Length frequency plots as joy divison plots
+make_joy_division_length <- TRUE
+
+
+# Base maps ---------------------------------------------------------------
+ai_east <- akgfmaps::get_base_layers(
+  select.region = "ai.east",
+  set.crs = "auto"
+)
+ai_central <- akgfmaps::get_base_layers(
+  select.region = "ai.central",
+  set.crs = "auto"
+)
+ai_west <- akgfmaps::get_base_layers(
+  select.region = "ai.west",
+  set.crs = "auto"
+)
+
+nstrata <- length(unique(floor(ai_east$survey.grid$STRATUM / 10)))
+
+# Aesthetic settings ------------------------------------------------------
+
+bubbletheme <- theme(
+  legend.position = "none",
+  panel.background = element_rect(
+    fill = "white",
+    colour = NA
+  ),
+  panel.border = element_rect(
+    fill = NA,
+    colour = "grey20"
+  ),
+  axis.text = element_text(size = 8),
+  strip.background = element_blank(),
+  strip.text = element_text(size = 10, face = "bold"),
+  legend.text = element_text(size = 10),
+  legend.background = element_rect(
+    colour = "transparent",
+    fill = "transparent"
+  ),
+  legend.key = element_rect(
+    colour = "transparent",
+    fill = "transparent"
+  ),
+  legend.box = "horizontal",
+  axis.title.x = element_blank(),
+  axis.title.y = element_blank(),
+  plot.title = element_text(margin = margin(b = -30))
+)
+
+
+linetheme <- theme_bw(base_size = 12)
+bartheme <- theme_classic2(base_size = 12) +
+  theme(strip.background = element_blank())
+
+# Palettes!
+# MetBrewer (dark colors)
+stratumpal <- lengthen_pal(
+  shortpal = MetBrewer::met.brewer(name = "Hokusai1", type = "continuous"),
+  x = 1:nstrata
+)
+
+# Palette for lines
+linecolor <- RColorBrewer::brewer.pal(n = 9, name = "Blues")[9]
+accentline <- RColorBrewer::brewer.pal(n = 9, name = "Blues")[8]
+
+# Palette for joy div plot
+joypal <- lengthen_pal(shortpal = RColorBrewer::brewer.pal(n = 9,name = "Blues"),x = 1:nyears)
+
+# Palette for species colors and fills
+#speciescolors <- nmfspalette::nmfs_palette("regional web")(nrow(report_species) + 1)
+speciescolors <- lengthen_pal(
+  shortpal = MetBrewer::met.brewer(name = "VanGogh2", type = "discrete", direction = -1),
+  x = 1:(nrow(report_species) + 1)
+)
+
+# 0. Static figure: INPFC areas ----------------------------------------------
+#**** TODO: Load png or whatever and put it here. Can I store it in a list?
+#* 
+
+
+# 1. Biomass index relative to LT mean ---------------------------------------
+
+if (make_biomass_timeseries) {
+  list_biomass_ts <- list()
+  for (i in 1:2) { #nrow(report_species)
+    sp <- report_species$species_code[i]
+    name_bms <- report_species$spp_name_informal[i]
+    
+    dat <- biomass_total %>%
+      filter(SPECIES_CODE == report_species$species_code[i])
+    lta <- mean(dat$TOTAL_BIOMASS)
+    
+    p1 <- dat %>%
+      ggplot(aes(x = YEAR, y = TOTAL_BIOMASS)) +
+      geom_hline(yintercept = lta, color = accentline, lwd = 0.7, lty = 2) +
+      geom_point(color = linecolor, size = 2) +
+      geom_errorbar(aes(ymin = MIN_BIOMASS, ymax = MAX_BIOMASS),
+                    color = linecolor,size=0.9,width=0.7) +
+      ylab("Estimated total biomass (mt)") +
+      xlab("Year") +
+      scale_y_continuous(labels = scales::label_comma()) +
+      linetheme
+    p1
+    
+    list_biomass_ts[[i]] <- p1
+    names(list_biomass_ts)[[i]] <- report_species$species_code[i]
+    
+    png(
+      filename = paste0(dir_out_figures, name_bms, "_", YEAR, "_biomass_ts.png"),
+      width = 7, height = 7, units = "in", res = 150
+    )
+    print(p1)
+    dev.off()
+  }
+  #
+}
+
+
+# 2. Catch composition -------------------------------------------------------
+if (make_catch_comp) {
+  head(biomass_total)
+  biomass_total_filtered <- biomass_total %>%
+    left_join(report_species,
+              by = c("SPECIES_CODE" = "species_code")
+    ) %>%
+    mutate(spp_name_informal = tidyr::replace_na(data = spp_name_informal, replace = "Other species"))
+  
+  biomass_total_filtered$spp_name_informal <- factor(biomass_total_filtered$spp_name_informal,
+                                                     levels = c(report_species$spp_name_informal, "Other species")
+  )
+  
+  p2 <- biomass_total_filtered %>%
+    ggplot(aes(fill = spp_name_informal, y = TOTAL_BIOMASS / 10e6, x = YEAR)) +
+    geom_bar(position = "stack", stat = "identity") +
+    scale_fill_manual("", values = speciescolors) +
+    xlab("Year") +
+    ylab(expression(paste("Total estimated biomass (\u00D7 ", 10^6, " mt)"))) +
+    scale_y_continuous(expand = c(0, 0)) +
+    bartheme
+  
+  png(
+    filename = paste0(dir_out_figures, YEAR, "_biomass_catchcomp.png"),
+    width = 7, height = 7, units = "in", res = 150
+  )
+  print(p2)
+  dev.off()
+}
+
+# 3. CPUE bubble maps ----------------------------------------------------------
+if (make_cpue_bubbles) {
+  list_cpue_bubbles <- list()
+  for (i in 1:nrow(report_species)) {
+    spbubble <- report_species$species_code[i]
+    namebubble <- report_species$spp_name_informal[i]
+    
+    # CPUE data
+    thisyrshauldata <- cpue_raw %>%
+      filter(year == maxyr & srvy == SRVY & species_code == spbubble) %>%
+      st_as_sf(
+        coords = c("longitude_dd", "latitude_dd"),
+        crs = "EPSG:4326"
+      ) %>%
+      st_transform(crs = ai_east$crs)
+    
+    # MAPS
+    p3a <- ggplot() +
+      geom_sf(
+        data = ai_east$survey.grid,
+        mapping = aes(
+          fill = factor(floor(STRATUM / 10)),
+          color = factor(floor(STRATUM / 10))
+        )
+      ) +
+      scale_fill_manual(values = stratumpal) +
+      scale_color_manual(values = stratumpal) +
+      geom_sf(data = ai_east$akland) +
+      geom_sf(data = thisyrshauldata, aes(size = cpue_kgha), alpha = 0.5) +
+      scale_size(limits = c(0,max(thisyrshauldata$cpue_kgha))) +
+      coord_sf(
+        xlim = ai_east$plot.boundary$x,
+        ylim = ai_east$plot.boundary$y
+      ) +
+      scale_x_continuous(breaks = ai_east$lon.breaks) +
+      scale_y_continuous(breaks = ai_east$lat.breaks) +
+      labs(subtitle = "Eastern Aleutians") +
+      bubbletheme
+    
+    p3b <- ggplot() +
+      geom_sf(
+        data = ai_central$survey.grid,
+        mapping = aes(
+          fill = factor(floor(STRATUM / 10)),
+          color = factor(floor(STRATUM / 10))
+        )
+      ) +
+      scale_fill_manual(values = stratumpal) +
+      scale_color_manual(values = stratumpal) +
+      geom_sf(data = ai_central$akland) +
+      geom_sf(data = thisyrshauldata, aes(size = cpue_kgha), alpha = 0.5) +
+      scale_size(limits = c(0,max(thisyrshauldata$cpue_kgha))) +
+      coord_sf(
+        xlim = ai_east$plot.boundary$x,
+        ylim = ai_east$plot.boundary$y
+      ) +
+      coord_sf(
+        xlim = ai_central$plot.boundary$x,
+        ylim = ai_central$plot.boundary$y
+      ) +
+      scale_x_continuous(breaks = ai_central$lon.breaks) +
+      scale_y_continuous(breaks = ai_central$lat.breaks) +
+      labs(subtitle = "Central Aleutians") +
+      bubbletheme
+    
+    p3c <- ggplot() +
+      geom_sf(
+        data = ai_west$survey.grid,
+        mapping = aes(
+          fill = factor(floor(STRATUM / 10)),
+          color = factor(floor(STRATUM / 10))
+        )
+      ) +
+      scale_fill_manual(values = stratumpal) +
+      scale_color_manual(values = stratumpal) +
+      geom_sf(data = ai_west$akland) +
+      geom_sf(data = thisyrshauldata, aes(size = cpue_kgha), alpha = 0.5) +
+      scale_size(limits = c(0,max(thisyrshauldata$cpue_kgha))) +
+      coord_sf(
+        xlim = ai_east$plot.boundary$x,
+        ylim = ai_east$plot.boundary$y
+      ) +
+      coord_sf(
+        xlim = ai_west$plot.boundary$x,
+        ylim = ai_west$plot.boundary$y
+      ) +
+      scale_x_continuous(breaks = ai_west$lon.breaks) +
+      scale_y_continuous(breaks = ai_west$lat.breaks) +
+      labs(subtitle = paste0(namebubble, " - Western Aleutians - ", YEAR)) +
+      bubbletheme
+    
+    toprow <- cowplot::plot_grid(p3c,NULL, rel_widths = c(2,1))
+    bottomrow <- cowplot::plot_grid(NULL,p3a,rel_widths = c(1,2))
+    final_obj <- cowplot::plot_grid(toprow,p3b,bottomrow,ncol=1)
+    
+    png(
+      filename = paste0(dir_out_figures, namebubble, "_",maxyr,"_bubble_example.png"),
+      width = 10, height = 10, units = "in", res = 200
+    )
+    print(final_obj)
+    
+    dev.off()
+    
+    list_cpue_bubbles[[i]] <- final_obj # save fig to list
+    
+  } #/end species loop
+}
+
+
+
+
+# Percent changes in biomass since last survey ----------------------------
+
+head(biomass_total)
+
+compare_tab <- biomass_total %>% 
+  filter(YEAR %in% c(maxyr, compareyr) & 
+           SPECIES_CODE %in% report_species$species_code) %>%
+  dplyr::select(YEAR, SPECIES_CODE, TOTAL_BIOMASS) %>%
+  dplyr::arrange(YEAR) %>%
+  tidyr::pivot_wider(names_from = YEAR,
+                     values_from = TOTAL_BIOMASS,
+                     names_prefix = "yr_") %>%
+  as.data.frame()
+
+compare_tab$percent_change <- round((compare_tab[,3] - compare_tab[,2]) / compare_tab[,2] * 100, digits = 1)
+names(compare_tab)
+
+compare_tab2 <- compare_tab %>%
+  left_join(report_species, by = c("SPECIES_CODE"="species_code")) %>%
+  arrange(-SPECIES_CODE) 
+
+write.csv(x = compare_tab2,
+          file = paste0(dir_out_chapters,"comparison_w_previous_year.csv"))
+
+
+# 5. Length frequency plots - joy division plots -----------------------------
+
+if (make_joy_division_length) {
+  list_joy_length <- list()
+  
+  
+  length <- read.csv(here::here("data", "local_racebase", "length.csv"))
+  
+  length2 <- length %>%
+    mutate(YEAR = stringr::str_extract(CRUISE, "^\\d{4}")) %>%
+    filter(REGION == SRVY)
+  
+  length3 <- length2 %>%
+    left_join(haul2, by = c("HAULJOIN", "YEAR", "CRUISEJOIN", "VESSEL", "CRUISE", "HAUL")) %>%
+    dplyr::select(VESSEL, YEAR, LENGTH, FREQUENCY, SEX, GEAR_DEPTH, STRATUM, SPECIES_CODE) %>%
+    left_join(region_lu, by = "STRATUM") %>%
+    mutate(Sex = case_when(
+      SEX == 1 ~ "Male",
+      SEX == 2 ~ "Female",
+      SEX == 3 ~ "Unsexed"
+    )) %>%
+    dplyr::select(-SEX, -MIN_DEPTH, -MAX_DEPTH)
+  for (i in 1:nrow(report_species)) {
+    joyplot <- length3 %>%
+      filter(SPECIES_CODE == report_species$species_code[i]) %>%
+      ggplot(aes(x = LENGTH, y = YEAR, group = YEAR, fill = YEAR)) +
+      geom_density_ridges() +
+      scale_y_discrete(limits = rev) +
+      facet_wrap(~Sex) +
+      xlab("Length(mm)") +
+      ylab("Year") +
+      theme_ridges() +
+      scale_fill_manual(values = joypal) +
+      labs(title = paste(report_species$spp_name_informal[i]))
+    
+    png(filename = paste0(
+      dir_out_figures, maxyr, "_",
+      report_species$spp_name_informal[i], "_joyfreqhist.png"
+    ), width = 12, height = 8, units = "in", res = 200)
+    print(joyplot)
+    dev.off()
+    
+    list_joy_length[[i]] <- joyplot
+  }
+  
+}
 
 # CPUE map ----------------------------------------------------------------
 # The function used to generate this CPUE map is Emily's "plot_idw_xbyx()"
@@ -42,11 +376,6 @@ cpue_res <- 0.1 # will take less time
   names(list_figures)[1] <- "POP" # NOTE: NEED TO MAKE THIS THE WHOLE LIST OF SPECIES
  
 
-# test
-# png(filename = paste0(dir_out_figures,"POP2017.png"), width = 8, height = 4.5,units = 'in',res = 200)
-# figure1
-# dev.off()
-
 
  
  if(print_figs){
@@ -55,12 +384,8 @@ cpue_res <- 0.1 # will take less time
  }
 
 
- # *** *** Save --------------------------------------------
+ # SAVE FIGURES -----------------------------------------------------------
  save(list_figures,
       file = paste0(dir_out_figures, "report_figures.rdata")
  )
- 
-
-# (optional) print out figures --------------------------------------------
-
  
