7a1517938bf373c67dfdd9e59faa440042ce81dd
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-10-31 08:33:50 -0700

    stylize data prep code

--- R/03_prep_data.R
+++ R/03_prep_data.R
@@ -1,12 +1,12 @@
 # 03_prep_data
-# Read in local copies of all the "stuff" needed for making tables and figs
+# Read in local copies of all the "stuff" needed for making tables and figs.
 # This takes a while because a lot of these are big tables.
 # Note: if you've downloaded all the local versions of the tables, you should be able to run this script without internet access or anything. If it fails, make an issue!
 
 # Tables from RACEBASE ----------------------------------------------------
 # Get species table
 if (SRVY == "AI") report_species <- read.csv("data/ai_report_specieslist.csv")
-report_species <- report_species %>% 
+report_species <- report_species %>%
   arrange(-species_code)
 
 # haul info (source: RACEBASE)
@@ -18,12 +18,13 @@ species_names <- common_names %>%
   janitor::clean_names() %>%
   dplyr::rename(scientific_name = species_name) %>%
   dplyr::select(-year_added) %>%
-  mutate(major_group = case_when(species_code >= 10000 & species_code <= 19999 ~ "Flatfish",
-                                 species_code >= 20000 & species_code <= 39999 ~ "Roundfish",
-                                 species_code >= 30000 & species_code <= 36999 ~ "Rockfish",
-                                 species_code >= 40000 & species_code <= 99990 ~ "Invertebrates",
-                                 species_code >= 00150 & species_code <= 00799 ~ "Chondrichthyans"
-                                )) # add column for species category
+  mutate(major_group = case_when(
+    species_code >= 10000 & species_code <= 19999 ~ "Flatfish",
+    species_code >= 20000 & species_code <= 39999 ~ "Roundfish",
+    species_code >= 30000 & species_code <= 36999 ~ "Rockfish",
+    species_code >= 40000 & species_code <= 99990 ~ "Invertebrates",
+    species_code >= 00150 & species_code <= 00799 ~ "Chondrichthyans"
+  )) # add column for species category
 
 # This year's haul data
 haul_maxyr <- haul %>%
@@ -52,11 +53,11 @@ cpue_raw <- x %>%
 
 
 # Biomass by stratum (source: AI or GOA schema)
-biomass_stratum <- read.csv(here::here("data","local_ai","biomass_stratum.csv")) 
+biomass_stratum <- read.csv(here::here("data", "local_ai", "biomass_stratum.csv"))
 # where biomass_stratum.csv is GOA.BIOMASS_STRATUM or AI.BIOMASS_STRATUM downloaded from Oracle as csv - janky but will have to work for now
 
 # Total biomass across survey area - currently reads from local copy; download/update to new one by running the setup script again and downloading fresh tables from oracle)
-biomass_total <- read.csv(here::here("data","local_ai","biomass_total.csv"))
+biomass_total <- read.csv(here::here("data", "local_ai", "biomass_total.csv"))
 
 # Station allocation table (source: AI or GOA schema)
 all_allocation <- read.csv("data/local_ai/ai_station_allocation.csv")
@@ -73,7 +74,7 @@ region_lu <- dat %>%
 
 
 # individual values needed for report (e.g., most abundant species) -------
-nyears <- length(unique(filter(haul, REGION==SRVY)$CRUISE))
+nyears <- length(unique(filter(haul, REGION == SRVY)$CRUISE))
 
 haul2 <- haul %>%
   mutate(YEAR = stringr::str_extract(CRUISE, "^\\d{4}")) %>%
@@ -86,7 +87,7 @@ nstations <- haul2 %>%
 
 nsuccessfulhauls <- haul2 %>%
   filter(ABUNDANCE_HAUL == "Y") %>%
-  nrow() #420 in 2018
+  nrow() # 420 in 2018
 
 
 # Lengths and notoliths sampled -------------------------------------------
@@ -104,40 +105,42 @@ specimen_maxyr <- S %>%
   mutate(YEAR = as.numeric(gsub("(^\\d{4}).*", "\\1", CRUISE))) %>%
   filter(YEAR == maxyr & REGION == SRVY)
 otos_collected <- specimen_maxyr %>%
-  filter(SPECIMEN_SAMPLE_TYPE==1) %>% # this means it's an oto collection
-  dplyr::left_join(haul_maxyr, by = c("CRUISEJOIN", "HAULJOIN", "HAUL",
-                                      "REGION", "VESSEL", "YEAR")) %>%
-  dplyr::left_join(region_lu,by = c("STRATUM")) %>%
-  group_by(INPFC_AREA , `Depth range`) %>%
-  dplyr::summarize("Number of otoliths collected"=n()) %>%
+  filter(SPECIMEN_SAMPLE_TYPE == 1) %>% # this means it's an oto collection
+  dplyr::left_join(haul_maxyr, by = c(
+    "CRUISEJOIN", "HAULJOIN", "HAUL",
+    "REGION", "VESSEL", "YEAR"
+  )) %>%
+  dplyr::left_join(region_lu, by = c("STRATUM")) %>%
+  group_by(INPFC_AREA, `Depth range`) %>%
+  dplyr::summarize("Number of otoliths collected" = n()) %>%
   ungroup()
-  
+
 total_otos <- sum(otos_collected$`Number of otoliths collected`)
 
 # get number of fish and invert spps
-catch <- read.csv("data/local_racebase/catch.csv",header = TRUE)
+catch <- read.csv("data/local_racebase/catch.csv", header = TRUE)
 
 
 # Species with highest est'd biomass --------------------------------------
 highest_biomass <- biomass_total %>%
-  filter(YEAR==maxyr & SURVEY==SRVY) %>%
+  filter(YEAR == maxyr & SURVEY == SRVY) %>%
   dplyr::slice_max(n = 50, order_by = TOTAL_BIOMASS, with_ties = FALSE) %>%
   janitor::clean_names() %>%
   dplyr::left_join(species_names)
 
-highest_biomass_flatfish <- highest_biomass %>% 
-  filter(major_group=="Flatfish")
+highest_biomass_flatfish <- highest_biomass %>%
+  filter(major_group == "Flatfish")
 
 highest_skates <- biomass_total %>%
-  filter(YEAR==maxyr & SURVEY==SRVY) %>%
+  filter(YEAR == maxyr & SURVEY == SRVY) %>%
   janitor::clean_names() %>%
   dplyr::left_join(species_names) %>%
-  filter(major_group=="Chondrichthyans") %>%
+  filter(major_group == "Chondrichthyans") %>%
   dplyr::slice_max(n = 3, order_by = total_biomass, with_ties = FALSE)
 
 # Stats for the species in report_spps ------------------------------------
 head(report_species)
-biomass_total %>% 
-  filter(YEAR==maxyr) %>% 
-  janitor::clean_names() %>% 
+biomass_total %>%
+  filter(YEAR == maxyr) %>%
+  janitor::clean_names() %>%
   right_join(report_species)
