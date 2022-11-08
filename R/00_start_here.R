93d8121db3ea0886649037c9efe252f5d39120d1
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-11-07 22:06:00 -0800

    cleanup code

--- R/00_start_here.R
+++ R/00_start_here.R
@@ -8,13 +8,14 @@ print_figs <- FALSE # Do you want to print out PNGs of each figure?
 pres_or_report <- "report" # if "pres" the scripts will only make a subset of the figures and tables, the ones that we show for the GPT meeting
 
 # When did you save the last version of the figures and tables you want to use?
-tabledate <- "2022-08-30"
-figuredate <- "2022-08-30"
+tabledate <- "2022-11-03"
+figuredate <- "2022-11-03"
 
 # Survey information ------------------------------------------------------
 survnumber <- "sixteenth" 
 dates_conducted <- "June 6th through August 14th, 2018" # Change this for the year!
 SRVY <- "AI" # Options: "GOA", "AI"
+
 YEAR <- maxyr
 vessel1 <- "FV Ocean Explorer"
 vessel2 <- "FV Alaska Provider"
@@ -23,11 +24,17 @@ captain2 <- "Loren Reynolds"
 
 ref_compareyr <- "@von_szalay_data_2017" # should be included in cite/bibliography.bib
 
-if (SRVY == "GOA"){dir_googledrive <- "1UAQKChSuKohsRJ5enOloHPk3qFtk5kVC"} 
-# Link to folder:  https://drive.google.com/drive/folders/1UAQKChSuKohsRJ5enOloHPk3qFtk5kVC 
+if (SRVY == "GOA") {
+  dir_googledrive <- "1UAQKChSuKohsRJ5enOloHPk3qFtk5kVC"
+  survname_long <- "Gulf of Alaska"
+}
+# Link to folder:  https://drive.google.com/drive/folders/1UAQKChSuKohsRJ5enOloHPk3qFtk5kVC
 # This is where all the text files live and are edited.
 
-if(SRVY =="AI"){dir_googledrive <- "11RBHMEQtkq4BsuzY7AeNdX8IQPr5bv_J"} 
+if (SRVY == "AI") {
+  dir_googledrive <- "11RBHMEQtkq4BsuzY7AeNdX8IQPr5bv_J"
+  survname_long <- "Aleutian Islands"
+}
 # Link to folder: https://drive.google.com/drive/folders/11RBHMEQtkq4BsuzY7AeNdX8IQPr5bv_J
 
 # Report info -------------------------------------------------------------
@@ -89,8 +96,16 @@ aa <- askYesNo(msg = "Do you want to build all the figures right now? NOTE: This
 if (aa) {
   source(here::here("R", "make_figures.R"))
 }else{
+  # Static map of region 
+  img1_path <- "img/AleutiansMap.png" 
+  img1 <- png::readPNG(img1_path)
+  
   load(file = paste0(
-    dir_in_figures,"report_figures.rdata")) # object: list_figures
+    dir_in_figures,"cpue_bubbles.rdata")) # object: list_cpue_bubbles
+  load(file = paste0(
+    dir_in_figures,"joy_length.rdata")) # object: list_joy_length
+  load(file = paste0(
+    dir_in_figures,"biomass_ts.rdata")) # object: list_biomass_ts
 }
 
 
@@ -98,155 +113,10 @@ if (aa) {
 
 rmarkdown::render(paste0(dir_markdown, "/PARENT.Rmd"),
                   output_dir = dir_out_chapters,
-                  output_file = paste0("PARENT.docx")
+                  output_file = "PARENT.docx"
 )
 
 
-# *** 06 - Results_spp ------------------------
-report_spp1 <- add_report_spp(
-  spp_info = spp_info,
-  spp_info_codes = "species_code",
-  report_spp = report_spp,
-  report_spp_col = "order",
-  report_spp_codes = "species_code",
-  lang = TRUE
-)
-
-for (jj in 1:length(unique(report_spp1$file_name)[!is.na(unique(report_spp1$file_name))])) {
-  print(paste0(jj, " of ", length(unique(report_spp1$file_name)[!is.na(unique(report_spp1$file_name))]), ": ", unique(report_spp1$file_name)[jj]))
-
-  cnt_chapt_content <- auto_counter(cnt_chapt_content)
-  filename00 <- paste0(cnt_chapt, "_spp_")
-  rmarkdown::render(paste0(dir_code, "/06_results_spp.Rmd"),
-    output_dir = dir_out_chapters,
-    output_file = paste0(
-      filename00, cnt_chapt_content, "_",
-      unique(report_spp1$file_name)[jj], ".docx"
-    )
-  )
-}
-
-# *** 09 - Appendix ------------------------
-cnt_chapt <- NMFSReports::auto_counter(cnt_chapt)
-cnt_chapt_content <- "001"
-filename0 <- paste0(cnt_chapt, "_appendix_")
-rmarkdown::render(paste0(dir_code, "/09_appendix.Rmd"),
-  output_dir = dir_out_chapters,
-  output_file = paste0(filename0, cnt_chapt_content, ".docx")
-)
-
-
-# *** 10 - Endmatter ------------------------
-cnt_chapt <- NMFSReports::auto_counter(cnt_chapt)
-cnt_chapt_content <- "001"
-filename0 <- paste0(cnt_chapt, "_endmatter_")
-rmarkdown::render(paste0(dir_code, "/10_endmatter.Rmd"),
-  output_dir = dir_out_chapters,
-  output_file = paste0(filename0, cnt_chapt_content, ".docx")
-)
-
-
-# *** *** - Figures and Tables ------------------------
-# - run figures and tables before each chapter so everything works smoothly
-if (FALSE) {
-  report_spp1 <- add_report_spp(
-    spp_info = spp_info,
-    spp_info_codes = "species_code",
-    report_spp = report_spp,
-    report_spp_col = "order",
-    report_spp_codes = "species_code",
-    lang = TRUE
-  )
-
-  # cnt_chapt<-auto_counter(cnt_chapt)
-  cnt_chapt_content <- "001"
-  filename0 <- paste0(cnt_chapt, "_")
-  rmarkdown::render(paste0(dir_code, "/figtab_pres.Rmd"),
-    output_dir = dir_out_ref,
-    output_file = paste0(filename0, cnt_chapt_content, ".docx")
-  )
-
-
-
-
-  for (jj in 1:length(unique(report_spp1$file_name))) {
-    print(paste0(jj, " of ", length(unique(report_spp1$file_name))))
-    start_time <- Sys.time()
-    # cnt_chapt<-auto_counter(cnt_chapt)
-    # cnt_chapt_content<-"001"
-    filename00 <- paste0(cnt_chapt, "_spp_")
-    rmarkdown::render(paste0(dir_code, "/figtab_spp_pres.Rmd"),
-      output_dir = dir_out_ref,
-      output_file = paste0(
-        filename00, cnt_chapt_content, "_",
-        unique(report_spp1$file_name)[jj], ".docx"
-      )
-    )
-    end_time <- Sys.time()
-    print(paste0(end_time - start_time))
-  }
-
-  save(list_figures,
-    file = paste0(dir_out_figures, "/report_figures_pres.rdata")
-  )
-
-  save(list_tables,
-    file = paste0(dir_out_tables, "/report_tables_pres.rdata")
-  )
-}
-
-load(file = paste0(dir_out_tables, "/report_figures_pres.rdata"))
-load(file = paste0(dir_out_tables, "/report_tables_pres.rdata"))
-
-# subtitle
-cruises_maxyr0 <- haul_cruises_vess_maxyr %>%
-  dplyr::filter(SRVY %in% c("NBS", "EBS")) %>%
-  dplyr::select(
-    "year", "survey_name", "vessel", "vessel_name",
-    "vessel_ital", "SRVY", "SRVY_long",
-    "start_date_cruise", "end_date_cruise",
-    "start_date_haul", "end_date_haul"
-  ) %>%
-  unique() %>%
-  group_by(
-    year, survey_name, vessel, vessel_name,
-    vessel_ital, SRVY, SRVY_long
-  ) %>%
-  dplyr::summarise(
-    start_date_cruise = min(start_date_cruise),
-    end_date_cruise = max(end_date_cruise),
-    start_date_haul = min(start_date_haul),
-    end_date_haul = max(end_date_haul)
-  ) %>%
-  dplyr::arrange(start_date_cruise)
-
-str <- paste0(
-  format(min(cruises_maxyr0$start_date_haul), format = "%B %d"),
-  " to ",
-  format(max(cruises_maxyr0$end_date_haul), format = "%B %d, %Y")
-)
-
-cnt_chapt <- auto_counter(cnt_chapt)
-cnt_chapt_content <- "001"
-filename0 <- paste0(cnt_chapt, "_presentation_")
-rmarkdown::render(paste0(dir_code, "/11_presentation.Rmd"),
-  output_dir = dir_out_chapters,
-  output_file = paste0(filename0, cnt_chapt_content, ".pptx")
-)
-
-
-
-# SAVE OTHER OUTPUTS -----------------------------------------------------------
-
-# save(list_figures,
-#      file=paste0(dir_out_figures, "/report_figures.rdata"))
-#
-# save(list_tables,
-#      file=paste0(dir_out_tables, "/report_tables.rdata"))
-
-save(list_equations,
-  file = paste0(dir_out_tables, "/report_equations.rdata")
-)
 
 # SAVE METADATA ----------------------------------------------------------------
 
