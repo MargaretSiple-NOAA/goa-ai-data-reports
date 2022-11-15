729987a500870ebbb4fe38bf8f14df6c43124b06
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-11-15 15:44:02 -0800

    add example, put x's where there were zero catches

--- R/markobubble.R
+++ R/markobubble.R
@@ -4,55 +4,50 @@
 #'
 #' @param yrs numeric vector of years for which you want plots. For data reports, this is `maxyr`.
 #' @param dat dataframe of cpue by tow. Columns must include year, lat, long, cpue_kgha. For data reports, this is very similar to/the same as the table `thisyrshauldata``
-#' @param lat
-#' @param lon
-#' @param year
-#' @param key.title
-#' @param row0
+#' @param key.title character - text for title of legend?
+#' @param row0 numeric. I don't really know, too lazy to remove. Default is 2.
 #' @param reg_dat sf object from calling akgfmaps::get_base_layers(). See example.
-#' @param dist_unit
-#' @param col_viridis
-#' @param plot_coldpool
-#' @param plot_stratum
-#' @param plot_bubble
+#' @param dist_unit string giving the units you want for distance in the scale bar
+#' @param col_viridis what viridis 
+#' @param plot_coldpool logical. plot outline of cold pool?
+#' @param plot_stratum logical. plot stratum lines?
 #'
-#' @return
+#' @return a ggplot object of a CPUE bubble map
 #' @export
 #'
 #' @example
-#'
-#'
-############## BELOW IS MEGSIE TESTING CODE FOR THE FUNCTION #################
-library(magrittr)
-reg_dat_ai <- akgfmaps::get_base_layers(select.region = "ai", set.crs = "EPSG:3338")
-reg_dat_ai$survey.area <- reg_dat_ai$survey.area %>%
-  dplyr::mutate(
-    SRVY = "AI",
-    color = scales::alpha(colour = "grey80", 0.7),
-    SURVEY = "Aleutian Islands"
-  )
-reg_dat <- reg_dat_ai
-# cpue_raw is generated in prep_data.R and is a summary of cpue by species and station
-spcode <- 30060
-thisyrshauldata <- cpue_raw %>%
-  mutate(cpue_kgha = cpue_kgkm2 * 100) %>%
-  filter(year == maxyr & survey == SRVY & species_code == spcode) %>%
-  st_as_sf(
-    coords = c("start_longitude", "start_latitude"), # TODO NEED TO CHANGE TO THE RIGHT COORDS
-    crs = "EPSG:4326"
-  ) %>%
-  st_transform(crs = reg_dat_ai$crs)
-dat <- thisyrshauldata
+#' library(magrittr)
+#' reg_dat_ai <- akgfmaps::get_base_layers(select.region = "ai", set.crs = "EPSG:3338")
+#' reg_dat_ai$survey.area <- reg_dat_ai$survey.area %>%
+#'   dplyr::mutate(
+#'     SRVY = "AI",
+#'     color = scales::alpha(colour = "grey80", 0.7),
+#'     SURVEY = "Aleutian Islands"
+#'   )
+#' reg_dat <- reg_dat_ai
+#' # cpue_raw is generated in prep_data.R and is a summary of cpue by species and station
+#' thisyrshauldata <- cpue_raw %>%
+#'   mutate(cpue_kgha = cpue_kgkm2 * 100) %>%
+#'   filter(year == maxyr & survey == SRVY & species_code == spcode) %>%
+#'   st_as_sf(
+#'     coords = c("start_longitude", "start_latitude"), # TODO NEED TO CHANGE TO THE RIGHT #' COORDS
+#'     crs = "EPSG:4326"
+#'   ) %>%
+#'   st_transform(crs = reg_dat_ai$crs)
+#'   fig <- plot_pa_xbyx(spcode=21921,dat = thisyrshauldata,yrs = c(2022),key.title = "",
+#'   row0 = 2,reg_dat = reg_dat_ai,dist_unit = "nm", # nautical milescol_viridis = "mako
+#'   ,plot_coldpool = FALSE,plot_stratum = FALSE)
+#'  png("Atka_bubble_2022.png",width = 8,height = 5.5,units = 'in',res = 200)
+#'  fig 
+#'  dev.off()
 
-
-##############################################################################
-
+#############################################################################
 plot_pa_xbyx <- function(spcode, # speciescode
                          dat = thisyrshauldata,
                          yrs = c(2022),
                          key.title = "",
                          row0 = 2,
-                         reg_dat,
+                         reg_dat = reg_dat_ai,
                          dist_unit = "nm", # nautical miles
                          col_viridis = "mako",
                          plot_coldpool = FALSE,
@@ -64,15 +59,23 @@ plot_pa_xbyx <- function(spcode, # speciescode
     geom_sf(
       data = reg_dat$akland,
       color = NA,
-      fill = "grey50"
+      fill = "grey40"
     ) +
     geom_sf(
-      data = thisyrshauldata, aes(size = ifelse(cpue_kgkm2 == 0, 0, cpue_kgkm2)),
+      data = filter(thisyrshauldata,cpue_kgkm2>0), 
+      aes(size = cpue_kgkm2),
       alpha = 0.5,
       color = mako(n = 1, begin = .25, end = .75)
     ) +
+    geom_sf( #x's for places where we sampled but didn't catch any of that species
+      data = filter(thisyrshauldata,cpue_kgkm2==0), 
+      alpha = 0.5,
+      color = "grey5",
+      shape = 4, 
+      size = 1
+    ) +
     scale_size_area(
-      name = legendtitle
+      name = legendtitle,max_size = 10
     )
 
   f2 <- f1 +
@@ -177,7 +180,6 @@ plot_pa_xbyx <- function(spcode, # speciescode
       legend.box = "horizontal"
     ) +
     labs(size = legendtitle)
-
 
   return(figure)
 }
