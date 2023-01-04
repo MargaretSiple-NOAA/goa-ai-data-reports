7c86412bf7f03feda6b75f8c779fe4c836f3528c
mcsiple-NOAA <margaret.siple@noaa.gov>
2023-01-03 16:23:53 -0800

    move vessel spec chunk to report settings so it's easier to automate

--- R/00_report_settings.R
+++ R/00_report_settings.R
@@ -48,3 +48,8 @@ report_title <- paste0(
 )
 report_authors <- "P. von Szalay, N. Raring, W. Palsson, B. Riggle, M. Siple"
 report_yr <- maxyr
+
+
+# Vessel specs ------------------------------------------------------------
+vessel_spec_chunk <-  "The Alaska Provider is 53.6 m in overall length (LOA) and is powered by two main engines with 2,200 continuous horsepower (HP). The Ocean Explorer is 47.2 m LOA with a 1,500 HP main engine.  The Alaska Provider is equipped with two forward and one stern net reel, but only the aft reel was used during the AI `r maxyr` bottom trawl survey. The Ocean Explorer is equipped with two forward net reels, both of which were used during the AI `r maxyr` bottom trawl survey."  
+
