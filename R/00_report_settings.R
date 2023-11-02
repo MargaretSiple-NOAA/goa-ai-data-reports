4a0d8793469867d2bc575d591f909b15d9f1317a
mcsiple-NOAA <margaret.siple@noaa.gov>
2023-11-02 15:31:43 -0700

    post-2022-release, update settings to be for GOA 2023 :tada:

--- R/00_report_settings.R
+++ R/00_report_settings.R
@@ -2,38 +2,36 @@
 
 # Report settings -------------------------------------------------------------
 usePNGPDF <- "png"
-maxyr <- 2022 # Change this for the year!
-minyr <- 1991 # This is the min year for the "data stanza" that has the 
-compareyr <- 2016 # Change this for the year!
+maxyr <- 2023 # Change this for the year!
+minyr <- 1991 # This is the min year for the "data stanza"
+compareyr <- 2021 # Change this for the year!
 pres_or_report <- "report" # if "pres" the scripts will only make a subset of the figures and tables, the ones that we show for the GPT meeting
 use_sql_cpue <- TRUE # Do we want to use CPUE values from BIOMASS_TOTAL or "build our own" from RACEBASE catch, haul, etc tables? The default, TRUE, uses CPUE values from BIOMASS_TOTAL.
-district_order <- c("Southern Bering Sea",
-                    "Eastern Aleutians",
-                    "Central Aleutians",
-                    "Western Aleutians") # order of districts in the results
 
 # When did you save the last version of the figures and tables you want to use?
-tabledate <- "2023-09-28" 
+tabledate <- "2023-09-28"
 figuredate <- "2023-09-28"
 reportvaluesdate <- "2023-09-28"
 
 # Survey information ------------------------------------------------------
 # charter start and end dates (From Ned: these dates should represent the inclusive vessel charter dates (we stagger start the vessels now) and not just the dates when we began and ended towing. The dates in the present report appear to capture the correct date range.)
 
-SRVY <- "AI" # Options: "GOA", "AI"
+SRVY <- "GOA" # Options: "GOA", "AI"
 
-if(SRVY=="AI"){
+if (SRVY == "AI") {
   dates_conducted <- "June 6th through August 14th, 2022" # Change this for the year!
-}else{
-  dates_conducted <- "May 18th through August 6th, 2023" 
+} else {
+  dates_conducted <- "May 18th through August 6th, 2023"
 }
 
+
+# Pre-assigned stations
 if (SRVY == "GOA") {
-  #all_allocation <- read.csv(here::here("data", "local_goa", "goa_station_allocation.csv"))
+  # all_allocation <- read.csv(here::here("data", "local_goa", "goa_station_allocation.csv"))
   preassignedstationstatement <- "This year, we pre-assigned XX% of the total XXX stations allocated as “new” meaning the each vessel had to trawl around a dozen previously untrawled stations last summer
 and this will become a permanent feature of our station allocations in the future."
-}else{
-  #all_allocation <- read.csv(here::here("data", "local_ai", "ai_station_allocation.csv"))
+} else {
+  # all_allocation <- read.csv(here::here("data", "local_ai", "ai_station_allocation.csv"))
   preassignedstationstatement <- ""
 }
 
@@ -60,12 +58,32 @@ if (SRVY == "AI") {
 }
 # Link to folder: https://drive.google.com/drive/folders/11RBHMEQtkq4BsuzY7AeNdX8IQPr5bv_J
 
+# Order of districts (for plots and tables) -------------------------------
+if (SRVY == "AI") {
+  district_order <- c(
+    "Southern Bering Sea",
+    "Eastern Aleutians",
+    "Central Aleutians",
+    "Western Aleutians"
+  ) # order of districts in the results
+}
+if (SRVY == "GOA") {
+  district_order <- c(
+    "Shumagin",
+    "Chirikof",
+    "Kodiak",
+    "Yakutat",
+    "Southeastern"
+  )
+}
+
+
 # Report info -------------------------------------------------------------
 report_title <- paste0(
   "Data Report: ", maxyr, " ", SRVY,
   " Bottom Trawl Survey"
 )
-report_authors <- "P. von Szalay, N. Raring, W. Palsson, B. Riggle, M. Siple"
+report_authors <- "M. Siple, P. von Szalay, N. Raring, W. Palsson, B. Riggle"
 report_yr <- maxyr
 
 
@@ -82,7 +100,7 @@ if (SRVY == "GOA") report_species <- read.csv(here::here("data", "goa_report_spe
 
 report_species <- report_species |>
   dplyr::arrange(-species_code) |>
-  dplyr::filter(report == 1) 
+  dplyr::filter(report == 1)
 
 # Reorder based on specified spps order
 report_species <- report_species[order(report_species$reportorder), ]
