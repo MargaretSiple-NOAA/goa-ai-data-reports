c0c73bc34c9244e5173d1962ac1d49e9781dcfab
mcsiple-NOAA <margaret.siple@noaa.gov>
2023-11-06 11:52:33 -0800

    generalize blurb and otolith target download from google drive

--- R/04_get_gdrive_chapters.R
+++ R/04_get_gdrive_chapters.R
@@ -21,13 +21,31 @@ for (i in 1:nrow(chaps)) {
 
 # Download species blurbs table -------------------------------------------
 blurbs_id_googledrive <- googledrive::as_id(dir_blurbs)
-blurbsheet <- googledrive::drive_ls(path = blurbs_id_googledrive, type = "spreadsheet")
-if(SRVY=="AI"){
-  googledrive::drive_download(file = googledrive::as_id(blurbsheet$id[1]),
-                              type = "csv",
-                              overwrite = TRUE,
-                              path = paste0("data", "/", blurbsheet$name[1]))
-}
+
+blurbsheet <- googledrive::drive_ls(
+  path = blurbs_id_googledrive,
+  pattern = paste0(SRVY, maxyr, "_SpeciesBlurbMiddleSentences"), type = "spreadsheet"
+)
+
+googledrive::drive_download(
+  file = googledrive::as_id(blurbsheet$id),
+  type = "csv",
+  overwrite = TRUE,
+  path = paste0("data", "/", blurbsheet$name)
+)
+
+
+# Download table with the oto target and collection numbers ---------------
+otosheet <- googledrive::drive_ls(path = blurbs_id_googledrive,
+                                  pattern = paste0(SRVY, maxyr, "_otolith_targets"),
+                                  type = "spreadsheet")
+
+googledrive::drive_download(
+  file = googledrive::as_id(otosheet$id),
+  type = "csv",
+  overwrite = TRUE,
+  path = paste0("data", "/", otosheet$name)
+)
 
 
 # Convert text files in gdrive directory into Rmd files -------------------
