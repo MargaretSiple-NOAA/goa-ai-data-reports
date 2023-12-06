b92f389990688e7eefe87b4c65db11b9c34a4692
mcsiple-NOAA <margaret.siple@noaa.gov>
2023-12-06 13:49:22 -0800

    add check for odd years but "AI" setting and vice versa. change settings to GOA

--- R/00_report_settings.R
+++ R/00_report_settings.R
@@ -2,21 +2,29 @@
 
 # Report settings -------------------------------------------------------------
 usePNGPDF <- "png"
-maxyr <- 2022 # Change this for the year!
+maxyr <- 2023 # Change this for the year!
 minyr <- 1991 # This is the min year for the "data stanza"
-compareyr <- 2018 # Change this for the year!
+compareyr <- 2021 # Change this for the year!
 pres_or_report <- "report" # if "pres" the scripts will only make a subset of the figures and tables, the ones that we show for the GPT meeting
 use_sql_cpue <- FALSE # Do we want to use CPUE values from BIOMASS_TOTAL or "build our own" from RACEBASE catch, haul, etc tables? The default, TRUE, uses CPUE values from BIOMASS_TOTAL.
 
 # When did you save the last version of the figures and tables you want to use?
-tabledate <-"2023-09-28"  #"2023-11-10"
-figuredate <- "2023-09-28"
-reportvaluesdate <- "2023-09-28"
+tabledate <-"2023-12-06"  #"2023-11-10"
+figuredate <- "2023-12-06"
+reportvaluesdate <- "2023-12-06"
 
 # Survey information ------------------------------------------------------
 # charter start and end dates (From Ned: these dates should represent the inclusive vessel charter dates (we stagger start the vessels now) and not just the dates when we began and ended towing. The dates in the present report appear to capture the correct date range.)
 
-SRVY <- "AI" # Options: "GOA", "AI"
+SRVY <- "GOA" # Options: "GOA", "AI"
+
+# Check survey year and region combo:
+if(maxyr %% 2 == 0 && SRVY == "GOA"){
+  print("Even year should not be for GOA survey. Double check your maxyr and SRVY designations in 00_report_settings.R")}
+if(maxyr %% 2 != 0 && SRVY =="AI"){
+  print("Odd year should not be used for AI survey. Double check your maxyr and SRVY designations in 00_report_settings.R")
+}
+
 
 if (SRVY == "AI") {
   dates_conducted <- "6 June through 14 August, 2022" # Change this for the year!
