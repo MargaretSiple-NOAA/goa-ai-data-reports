e647b936dde7bab2e9e9fd7425642e552de83fcf
mcsiple-NOAA <margaret.siple@noaa.gov>
2024-02-05 14:45:16 -0800

    update appendices for GOA 2023

--- R/12_make_appendices.R
+++ R/12_make_appendices.R
@@ -8,8 +8,8 @@
 
 # Appendix B --------------------------------------------------------------
 # Species encountered
-source("R/06_prep_data.R")
-source("R/06a_appendix_b.R") #makes the table appB
+# source("R/06_prep_data.R")
+# source("R/06a_appendix_b.R") #makes the table appB
 
 # Now just put in the flextable so that species_2 is in italics and new_suffix is not in italics. It should work for pretty much all the cases!
 rmarkdown::render(paste0(dir_markdown, "/APPENDIXB.Rmd"),
@@ -20,15 +20,8 @@ rmarkdown::render(paste0(dir_markdown, "/APPENDIXB.Rmd"),
 # Appendix C --------------------------------------------------------------
 # Length-weight parameters
 # This file is created by Paul in 2022; eventually we can automate
-# All 2022 apendices can be found in G:\ALEUTIAN\AI 2022\DataReport\Appendices\
+# All 2022 appendices can be found in G:\ALEUTIAN\AI 2022\DataReport\Appendices\
 
 # Appendix D --------------------------------------------------------------
 # Bottom and surface temps
-# This file is created by Paul in 2022; will take more work to automate because it contains materials from ArcGIS and we'll have to turn them into maps in R.
-
-
-# Appendix E --------------------------------------------------------------
-# Ops manual
-# Get Word doc from G:/ALEUTIAN/AI 2022/Ops Plan/2022 AI Scientific Operations Plan.docx
-file.copy(from = "G:/ALEUTIAN/AI 2022/Ops Plan/2022 AI Scientific Operations Plan.docx",
-          to = paste0(appendix_dir,"Appendix E.docx"))
+# This file is created by Paul in 2022; will take more work to automate because it contains materials from ArcGIS and we'll have to turn them into maps in R. 
