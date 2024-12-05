feff775d75f3e0757e9ea1b83425668bd3271923
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2024-12-05 12:46:03 -0800

    Add placeholder for appendix A2, but checking w Paul for csv

--- R/06a_appendix_a.R
+++ R/06a_appendix_a.R
@@ -40,4 +40,23 @@ a1 <- area |>
   dplyr::rename("Depth range (m)" = "DEPTH_RANGE",
                 "Stratum number" = "AREA_ID",
                 "Stratum name" = "AREA_NAME")
-  
+
+
+# Table A2: summary codes for strata.  ------------------------------------
+# THis is static, so I'm just loading it from saved data.
+# if(SRVY=="GOA" & maxyr < 2025){
+#   a2 <- read.csv("data/GOA_alllyrs_appendix_a2.csv")
+# }
+# 
+# if(SRVY=="AI"){
+#   stratum_summaries <- area |>
+#     dplyr::filter(AREA_TYPE %in% c("STRATUM")) |>
+#     dplyr::mutate(DEPTH_RANGE = paste(DEPTH_MIN_M, "-", DEPTH_MAX_M)) |>
+#     dplyr::select(AREA_ID, DEPTH_RANGE, AREA_NAME, AREA_KM2) 
+#   
+#   a2 <- area |>
+#     dplyr::filter(AREA_TYPE %in% c("STRATUM")) |>
+#     dplyr::mutate(DEPTH_RANGE = paste(DEPTH_MIN_M, "-", DEPTH_MAX_M)) |>
+#     dplyr::select(AREA_ID, DEPTH_RANGE, AREA_NAME, AREA_KM2)
+# }
+#   
