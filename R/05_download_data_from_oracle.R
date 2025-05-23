a4329fa758109a93160afb657336759f692e7977
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2025-05-22 22:11:07 -0700

    tiny edits

--- R/05_download_data_from_oracle.R
+++ R/05_download_data_from_oracle.R
@@ -201,7 +201,6 @@ write.csv(x = a, "./data/local_gap_products/stratum_groups.csv", row.names = FAL
 a <- RODBC::sqlQuery(channel, "SELECT * FROM GAP_PRODUCTS.SPECIES_YEAR")
 write.csv(x = a, "./data/local_gap_products/species_year.csv", row.names = FALSE)
 
-
 print("Finished downloading GAP_PRODUCTS tables.")
 
 # Ex-vessel prices --------------------------------------------------------
