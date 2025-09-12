199c394c2d88c897b04d79049f22504e137106d6
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2025-09-12 14:45:20 -0700

    udpate areas for top_CPUE table and others #100

--- R/07_make_tabs.R
+++ R/07_make_tabs.R
@@ -47,7 +47,7 @@ area_gp <- read.csv("data/local_gap_products/area.csv")
 
 if(SRVY=="GOA"){
 area_gp_reg_area <- area_gp |>
-  dplyr::filter(AREA_TYPE %in% c("NMFS STATISTICAL AREA", "REGULATORY AREA") &
+  dplyr::filter(AREA_TYPE %in% c("NMFS STATISTICAL AREA", "REGION") &
     DESIGN_YEAR == ifelse(maxyr >= 2025, 2025, 1984))}else{
       area_gp_reg_area <- area_gp |>
         dplyr::filter(AREA_TYPE %in% c("INPFC", "REGION") &
@@ -66,21 +66,22 @@ top_CPUE <- biomass_subarea |>
   dplyr::filter(YEAR == maxyr & grepl(pattern = "[0-9]", x = SPECIES_CODE)) |>
   dplyr::filter(SPECIES_CODE < 40001) |> # take out inverts
   dplyr::right_join(area_gp_reg_area, by = c("SURVEY_DEFINITION_ID","AREA_ID")) |>
+  #dplyr::filter(AREA_TYPE == "NMFS STATISTICAL AREA") |>
   dplyr::select(AREA_NAME, N_HAUL, SPECIES_CODE, CPUE_KGKM2_MEAN) |>
-  dplyr::rename("INPFC_AREA" = AREA_NAME) |>
+  dplyr::rename("NMFS_STATISTICAL_AREA" = AREA_NAME) |>
   dplyr::mutate(wgted_mean_cpue_kgha = CPUE_KGKM2_MEAN / 100) |> # convert to hectares
-  group_by(INPFC_AREA) |>
+  group_by(NMFS_STATISTICAL_AREA) |>
   dplyr::slice_max(n = topn, order_by = wgted_mean_cpue_kgha, with_ties = FALSE) |>
   left_join(species_names, by = c("SPECIES_CODE" = "species_code")) |>
   dplyr::rename(
     "species_code" = SPECIES_CODE
   ) |>
-  dplyr::ungroup() |>
-  dplyr::mutate(INPFC_AREA = ifelse(INPFC_AREA == "All", "All areas", INPFC_AREA))
+  dplyr::ungroup() #|>
+  #dplyr::mutate(NMFS_STATISTICAL_AREA = ifelse(NMFS_STATISTICAL_AREA == "All", "All areas", INPFC_AREA))
 
 
-top_CPUE <- top_CPUE %>%
-  arrange(factor(INPFC_AREA, levels = c(district_order, "All")))
+top_CPUE <- top_CPUE |>
+  arrange(factor(NMFS_STATISTICAL_AREA, levels = c(district_order, "All")))
 
 
 
@@ -92,86 +93,88 @@ write.csv(
 
 # Stations allocated, attempted, succeeded --------------------------------
 
-attempted <- haul_maxyr %>%
-  group_by(STRATUM) %>%
-  distinct(STATIONID) %>% # how many stations were attempted sampled?
-  ungroup() %>%
-  left_join(region_lu) %>%
-  group_by(INPFC_AREA, `Depth range`) %>%
-  dplyr::count(name = "attempted") %>%
+attempted <- haul |>
+  mutate(YEAR = as.numeric(gsub("(^\\d{4}).*", "\\1", CRUISE))) |> # extract year
+  filter(REGION == SRVY & YEAR == maxyr) |>
+  group_by(STRATUM) |>
+  distinct(STATIONID) |> # how many stations were attempted sampled?
+  ungroup() |>
+  left_join(region_lu) |>
+  group_by(REGULATORY_AREA_NAME, `Depth range`) |>
+  dplyr::count(name = "attempted") |>
   ungroup()
 
-succeeded <- haul_maxyr %>%
-  group_by(STRATUM) %>%
-  filter(ABUNDANCE_HAUL == "Y") %>% # filter to successful hauls
-  distinct(STATIONID) %>% # how many stations were sampled?
-  ungroup() %>%
-  left_join(region_lu) %>%
-  group_by(INPFC_AREA, `Depth range`) %>%
-  dplyr::count(name = "succeeded") %>%
+succeeded <- haul_maxyr |>
+  group_by(STRATUM) |>
+  filter(ABUNDANCE_HAUL == "Y") |> # filter to successful hauls
+  distinct(STATIONID) |> # how many stations were sampled?
+  ungroup() |>
+  left_join(region_lu) |>
+  group_by(REGULATORY_AREA_NAME, `Depth range`) |>
+  dplyr::count(name = "succeeded") |>
   ungroup()
 
-inpfc_depth_areas <- region_lu %>%
-  distinct(INPFC_AREA, STRATUM, AREA, `Depth range`) %>%
-  group_by(INPFC_AREA, `Depth range`) %>%
-  dplyr::summarize(AREA = sum(AREA)) %>%
+nmfs_depth_areas <- region_lu |>
+  distinct(REGULATORY_AREA_NAME, STRATUM, AREA, `Depth range`) |>
+  group_by(REGULATORY_AREA_NAME, `Depth range`) |>
+  dplyr::summarize(AREA = sum(AREA)) |>
   ungroup()
 
 
-piece1 <- all_allocation %>%
-  filter(YEAR == maxyr & SURVEY == SRVY) %>%
-  left_join(region_lu, by = c("SURVEY", "STRATUM")) %>%
-  group_by(INPFC_AREA, `Depth range`) %>%
-  dplyr::count(name = "allocated") %>%
-  ungroup() %>%
-  left_join(attempted) %>%
-  left_join(succeeded) %>%
+piece1 <- all_allocation |>
+  filter(YEAR == maxyr & SURVEY == SRVY) |>
+  left_join(region_lu, by = c("SURVEY", "STRATUM")) |>
+  group_by(INPFC_AREA, `Depth range`) |>
+  dplyr::count(name = "allocated") |>
+  ungroup() |>
+  left_join(attempted) |>
+  left_join(succeeded) |>
   left_join(inpfc_depth_areas)
 
-depth_areas <- piece1 %>%
-  group_by(INPFC_AREA) %>%
+depth_areas <- piece1 |>
+  group_by(INPFC_AREA) |>
   dplyr::summarize(
     AREA = sum(AREA),
     allocated = sum(allocated),
     attempted = sum(attempted),
     succeeded = sum(succeeded)
-  ) %>%
-  ungroup() %>%
+  ) |>
+  ungroup() |>
   mutate(`Depth range` = "All depths")
 
-allocated_prep <- piece1 %>%
-  bind_rows(depth_areas) %>%
-  arrange(INPFC_AREA, `Depth range`) %>%
-  dplyr::arrange(factor(INPFC_AREA, levels = district_order)) %>%
-  mutate(stations_per_1000km2 = (succeeded / AREA) * 1000) %>%
+allocated_prep <- piece1 |>
+  bind_rows(depth_areas) |>
+  arrange(INPFC_AREA, `Depth range`) |>
+  dplyr::arrange(factor(INPFC_AREA, levels = district_order)) |>
+  mutate(stations_per_1000km2 = (succeeded / AREA) * 1000) |>
   mutate(
     AREA = round(AREA, digits = 1),
     stations_per_1000km2 = round(stations_per_1000km2, digits = 2)
   )
 
 
-all_areas <- allocated_prep %>%
-  filter(`Depth range` != "All depths") %>%
-  group_by(`Depth range`) %>%
+all_areas <- allocated_prep |>
+  filter(`Depth range` != "All depths") |>
+  group_by(`Depth range`) |>
   dplyr::summarize(
     allocated = sum(allocated),
     attempted = sum(attempted),
     succeeded = sum(succeeded),
     AREA = sum(AREA)
-  ) %>%
-  dplyr::mutate(stations_per_1000km2 = round((succeeded / AREA) * 1000, digits = 2)) %>%
-  ungroup() %>%
+  ) |>
+  dplyr::mutate(stations_per_1000km2 = round((succeeded / AREA) * 1000, digits = 2)) |>
+  ungroup() |>
   tibble::add_column(INPFC_AREA = "All areas", .before = "Depth range")
 
-all_areas_depths <- all_areas %>%
-  dplyr::summarize(across(allocated:AREA, sum)) %>%
-  tibble::add_column(`Depth range` = "All depths", .before = "allocated") %>%
-  ungroup() %>%
-  mutate(stations_per_1000km2 = succeeded / AREA) %>%
+all_areas_depths <- all_areas |>
+  dplyr::summarize(across(allocated:AREA, sum)) |>
+  tibble::add_column(`Depth range` = "All depths", .before = "allocated") |>
+  ungroup() |>
+  mutate(stations_per_1000km2 = succeeded / AREA) |>
   tibble::add_column(INPFC_AREA = "All areas", .before = "Depth range")
 
-allocated_sampled <- bind_rows(allocated_prep, all_areas, all_areas_depths) %>%
-  dplyr::arrange(factor(INPFC_AREA, levels = c(district_order, "All areas"))) %>%
+allocated_sampled <- bind_rows(allocated_prep, all_areas, all_areas_depths) |>
+  dplyr::arrange(factor(INPFC_AREA, levels = c(district_order, "All areas"))) |>
   dplyr::mutate(`Depth range` = gsub(" m", "", `Depth range`))
 
 colnames(allocated_sampled) <- c(
@@ -265,24 +268,24 @@ print("Done creating Table 4s")
 
 
 # Comparison of biomass between maxyr and compareyr -----------------------
-compare_tab <- biomass_total %>%
+compare_tab <- biomass_total |>
   filter(YEAR %in% c(maxyr, compareyr) &
-           SPECIES_CODE %in% report_species$species_code) %>%
-  dplyr::select(YEAR, SPECIES_CODE, BIOMASS_MT) %>%
-  dplyr::arrange(YEAR) %>%
+           SPECIES_CODE %in% report_species$species_code) |>
+  dplyr::select(YEAR, SPECIES_CODE, BIOMASS_MT) |>
+  dplyr::arrange(YEAR) |>
   tidyr::pivot_wider(
     names_from = YEAR,
     values_from = BIOMASS_MT,
     names_prefix = "yr_"
-  ) %>%
+  ) |>
   as.data.frame()
 
 compare_tab$percent_change <- round((compare_tab[, 3] - compare_tab[, 2]) / compare_tab[, 2] * 100, digits = 1)
 names(compare_tab)
 
-compare_tab2 <- compare_tab %>%
-  left_join(report_species, by = c("SPECIES_CODE" = "species_code")) %>%
-  #arrange(-SPECIES_CODE) %>%
+compare_tab2 <- compare_tab |>
+  left_join(report_species, by = c("SPECIES_CODE" = "species_code")) |>
+  #arrange(-SPECIES_CODE) |>
   dplyr::select(-(presentation:reportorder))
 
 write.csv(
