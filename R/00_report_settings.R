44b976026a73f7b0247e87b4b4a54149b38cbdef
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2026-01-13 14:53:21 -0800

    Make table font easier to adjust (set for whole doc)

--- R/00_report_settings.R
+++ R/00_report_settings.R
@@ -8,7 +8,7 @@ compareyr <- 2023 # Change this for the year!
 pres_or_report <- "report" # if "pres" the scripts will only make a subset of the figures and tables, the ones that we show for the GPT meeting
 use_gapindex <- FALSE # If TRUE will calculate total biomass and cpue_raw using the gapindex pkg. If FALSE, will use GAP_PRODUCTS schema.
 complexes <- TRUE # If TRUE will produce figures and tables for species complexes in addition to the basic single species (for AI this is OROX, REBS, OFLATS)
-
+tablefont <- "Arial"
 
 # Survey information ------------------------------------------------------
 # charter start and end dates (From Ned: these dates should represent the inclusive vessel charter dates (we stagger start the vessels now) and not just the dates when we began and ended towing. The dates in the present report appear to capture the correct date range.)
