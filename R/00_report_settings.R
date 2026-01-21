358343aa1165d6423b419e9aed104a29f9c66bef
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2026-01-20 17:18:05 -0800

    adjust text to include 2025 and onwards redesign methods if the year requires it

--- R/00_report_settings.R
+++ R/00_report_settings.R
@@ -9,6 +9,7 @@ pres_or_report <- "report" # if "pres" the scripts will only make a subset of th
 use_gapindex <- FALSE # If TRUE will calculate total biomass and cpue_raw using the gapindex pkg. If FALSE, will use GAP_PRODUCTS schema.
 complexes <- TRUE # If TRUE will produce figures and tables for species complexes in addition to the basic single species (for AI this is OROX, REBS, OFLATS)
 tablefont <- "Arial"
+redesign <- ifelse(maxyr>=2025 & SRVY=="GOA", TRUE, FALSE)
 
 # Survey information ------------------------------------------------------
 # charter start and end dates (From Ned: these dates should represent the inclusive vessel charter dates (we stagger start the vessels now) and not just the dates when we began and ended towing. The dates in the present report appear to capture the correct date range.)
