b28fcdeaff7f9e0e63a0f7d30d190cc0b9d4fcde
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2026-03-23 17:23:07 -0700

    fix sql script for n of surveys #111

--- sql/number_of_cruises.sql
+++ sql/number_of_cruises.sql
@@ -1,16 +1,7 @@
---to help figure out count of multi species survey
-select distinct cruise, gear, accessories
+select floor(cruise/100) year, avg(gear_temperature) mean_temp
 from racebase.haul
 where region = 'GOA'
 and abundance_haul = 'Y'
-order by cruise, accessories
-/
-
-select distinct cruise, gear, accessories
-from racebase.haul
-where region = 'GOA'
-and abundance_haul = 'Y'
-and gear = 172
-and accessories in (64, 129)
-order by cruise, accessories
+group by floor(cruise/100)
+order by floor(cruise/100)
 /
